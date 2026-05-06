2ed4a8996d5c33dded2b121b9dbd98acab8798a4
Olivier REMY <olivier.remyy@gmail.com>
2026-05-06 10:06:07 +0200

    display hidden files in telescope

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -10,6 +10,22 @@ return {
         cmd = "Cheatsheet",
     },
 
+    -- Telescope : inclure les fichiers cachés dans find_files / live_grep (sauf .git/)
+    {
+        "nvim-telescope/telescope.nvim",
+        opts = {
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    file_ignore_patterns = { "^.git/" },
+                },
+                live_grep = {
+                    additional_args = { "--hidden", "--glob", "!**/.git/*" },
+                },
+            },
+        },
+    },
+
     -- Navigation transparente entre panes tmux et splits neovim (Ctrl+h/j/k/l)
     {
         "christoomey/vim-tmux-navigator",
